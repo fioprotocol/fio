ca5b9981927fa8c1b1a784e14abe2854971c659d
dapixuser <androulidakisa@gmail.com>
2018-09-07 04:28:08 +0000

    Change filenames

--- contracts/fio.name/fio.name.cpp
+++ contracts/fio.name/fio.name.cpp
@@ -5,7 +5,8 @@
  *  @copyright Dapix
  *
  *  Changes:
- *  
+ *  Adam Androulidakis 9-6-2018
+ *  Ciju John 9-5-2018
  *  Adam Androulidakis  8-31-2018
  *  Ciju John  8-30-2018
  *  Adam Androulidakis  8-29-2019
@@ -13,116 +14,148 @@
 
 #include "fio.name.hpp"
 
-namespace fio {
-
-    class FioName : public contract //, token(_self, _self)
-	{
-    private:
+namespace fioio{
+	
+	 
+    class FioNameLookup : public contract { 
+		private:
         configs config;
         domains_table domains;
         fionames_table fionames;
 		account_name owner;
 		
 
+        // Currently supported chains
+        enum  class chain_type {
+            BTC=0, ETH=1, EOS=2, NONE=3
+        };
+        const std::vector<std::string> chain_str {"BTC", "ETH", "EOS"};
+
     public:
-        FioName(account_name self)
+        FioNameLookup(account_name self)
         : contract(self), config(self, self), domains(self, self), fionames(self, self) {owner=self;}
 
+    
         // @abi action
-        void registerName (const string &name) {
+        void registername(const string &name) {
             require_auth(owner);
 
-			
             string domain = nullptr;
-            string domainName = nullptr;
-            
-			//determine if there is dot in the name entered
-			// Weak input validation, only enter string like Adam or Adam.Fio
-			// TODO: Stronger input validation
-			int pos = name.find('.');
+            string fioname = domain;
 			
+            int pos = name.find('.');
             if (pos == string::npos) {
                 domain = name;
-            }
-			else {
-                domainName = name.substr(0, pos);
+            } else {
+                fioname = name.substr(0, pos);
                 domain = name.substr(pos + 1, string::npos);
             }
 
-            print("Name: ", domainName, ", Domain: ", domain, "\n");
-			// print("Name: ", (domainName), ", Domain: ", (domain), "\n");
+            print("fioname: ", fioname, ", Domain: ", domain, "\n");
 
             uint64_t domainHash = ::eosio::string_to_name(domain.c_str());
-            if (domainName.empty()) { // domain 
-                
-				// check for domain availability
+            if (fioname.empty()) { // domain register
+                // check for domain availability
                 print(", Domain hash: ", domainHash, "\n");
                 auto domains_iter = domains.find(domainHash);
                 eosio_assert(domains_iter == domains.end(), "Domain is already registered.");
+                // check if callee has requisite dapix funds.
 
-                // check if callee has requisite FIO funds.
-				// Do something
-				
-				
-				
                 // Issue, create and transfer nft domain token
-				
-				
                 domains.emplace(_self, [&](struct domain &d) {
                     d.name=domain;
                     d.domainhash=domainHash;
                 });
                 // Add domain entry in domain table
-            } 
-			// If a domain is not entered, add a name
-			else { // fioname 
-				
-				
-                // check if domain exists.
+            } else { // fioname register
+                
+				// check if domain exists.
                 auto domains_iter = domains.find(domainHash);
                 eosio_assert(domains_iter != domains.end(), "Domain not yet registered.");
-				
-                // check if domain permission is valid.
-				
-				// does callee account have perms??
-				
-				
-				// Maybe do checking and assigning under registerName ?
-				
-                // check if fioname is available
-                uint64_t accountHash = ::eosio::string_to_name(domainName.c_str());
-				
-				string pubKey =  "FIO:EOS78vWPM6dHgoaYXxU3jVtVCb6fRQjy3otLNuZ2WmkAGEmCkfQwL\nBTC:16ZYw7zXbCRAgkA2oPEij5wR9UKaSG5dbb\nBCH:16ZYw7zXbCRAgkA2oPEij5wR9UKaSG5dbb\n"; //temporary device - value is not yet passed to method
-				uint64_t pubKeyHash = ::eosio::string_to_name(pubKey.c_str()); 
-				
-                print("Name hash: ", accountHash, ", Domain has: ", domainHash, "\n");
-                auto fioname_iter = fionames.find(accountHash);
-                eosio_assert(fioname_iter == fionames.end(), "Name is already registered.");
                 
-				// check if callee has requisite FIO funds.
+				// check if domain permission is valid.
+                
+				// check if fioname is available
+				uint64_t nameHash = ::eosio::string_to_name(name.c_str());
+                print("Name hash: ", nameHash, ", Domain has: ", domainHash, "\n");
+                auto fioname_iter = fionames.find(nameHash);
+                eosio_assert(fioname_iter == fionames.end(), "Fioname is already registered.");
+                
+				// For testing, single public address string
+				string pubAddress =  "FIO:EOS78vWPM6dHgoaYXxU3jVtVCb6fRQjy3otLNuZ2WmkAGEmCkfQwL\nBTC:16ZYw7zXbCRAgkA2oPEij5wR9UKaSG5dbb\nBCH:16ZYw7zXbCRAgkA2oPEij5wR9UKaSG5dbb\n"; //temporary device - value is not yet passed to method
+				uint64_t pubAddressHash = ::eosio::string_to_name(pubAddress.c_str()); 
+				
+				
+				// check if callee has requisite dapix funds.
 				// DO SOMETHING
 				
                 // Issue, create and transfer fioname token
-                // DO SOMETHING
+				// DO SOMETHING
 				
-				// Add account entry in fionames table
+                // Add fioname entry in fionames table
                 fionames.emplace(_self, [&](struct fioname &a){
-                    a.name=domainName;
-                    a.namehash=accountHash;
+                    a.name=name;
+                    a.namehash=nameHash;
                     a.domain=domain;
                     a.domainhash=domainHash;
-					a.pubKey = pubKey;
-					a.pubKeyHash = pubKeyHash;
-		
+					a.pubAddress = pubAddress;
+					a.pubAddressHash = pubAddressHash;
+					
+                    a.addresses = vector<string>(chain_str.size(), "");
                 });
-            }
-        } //
-
-        // @abi action
-      	void addkey(const string &key, const string &chain) {
-			// Do something
-			eosio_assert(1,"This function has not yet been implemented");
+            } // else
         }
-    };
-    EOSIO_ABI( FioName, (registerName)(addkey) )
+
+        /***
+         * Convert chain name to chain type. Returns chain_type::NONE if no match.
+         *
+         * @param chain The chain name e.g. "BTC"
+         * @return
+         */
+        inline chain_type str_to_chain_type(const string &chain) {
+
+            print("size: ", chain_str.size(), "\n");
+            for (int i = 0; i < chain_str.size(); i++) {
+                print("chain: ", chain, ", chain_str: ", chain_str[i], "\n");
+                if (chain == chain_str[i]) {
+                    print("Found supported chain.", "\n");
+                    return static_cast<chain_type>(i);
+                }
+            }
+            return chain_type::NONE;
+        }
+
+        /***
+         * Given a fio user name, chain name and chain specific address will attach address to the user's FIO fioname.
+         *
+         * @param fio_user_name The FIO user name e.g. "adam.fio"
+         * @param chain The chain name e.g. "btc"
+         * @param address The chain specific user address
+         */
+        // @abi action
+        void addaddress(const string &fio_user_name, const string &chain, const string &address) {
+            eosio_assert(!fio_user_name.empty(), "FIO user name cannot be empty.");
+            eosio_assert(!chain.empty(), "Chain cannot be empty.");
+            eosio_assert(!address.empty(), "Chain address cannot be empty.");
+
+            // validate chain is supported. This is a case insensitive check.
+            string my_chain=chain;
+            transform(my_chain.begin(), my_chain.end(), my_chain.begin(), ::toupper);
+            print("Validating chain support: ", my_chain, "\n");
+            chain_type c_type= str_to_chain_type(my_chain);
+            eosio_assert(c_type != chain_type::NONE, "Supplied chain isn't supported..");
+
+            // validate fio fioname exists
+            uint64_t nameHash = ::eosio::string_to_name(fio_user_name.c_str());
+            auto fioname_iter = fionames.find(nameHash);
+            eosio_assert(fioname_iter != fionames.end(), "fioname not registered.");
+
+            // insert/update <chain, address> pair
+            fionames.modify(fioname_iter, _self, [&](struct fioname &a) {
+                a.addresses[static_cast<int>(c_type)] = address;
+            });
+        }
+    }; // class FioNameLookup
+
+    EOSIO_ABI( FioNameLookup, (registername)(addaddress) )
 }
--- contracts/fio.name/fio.name.hpp
+++ contracts/fio.name/fio.name.hpp
@@ -2,55 +2,55 @@
  *  Description: FioName smart contract allows issuance of unique domains and names for easy public address resolution
  *  @author Adam Androulidakis
  *  @file fio.name.hpp
- *  @copyright defined in eos/LICENSE.txt
+ *  @copyright Dapix
  *
  *  Changes:
- *  
+ *  Adam Androulidakis 9-6-2018
+ *  Ciju John 9-5-2018
  *  Adam Androulidakis  8-31-2018
  *  Ciju John  8-30-2018
  *  Adam Androulidakis  8-29-2019
  */
  
+
 #pragma once
 
 #include <eosiolib/eosio.hpp>
 #include <string>
 #include <eosiolib/singleton.hpp>
-
-//asset header is current unused, this will need to be
-//commented out for FioName to inherit token properties
-//#include <eosiolib/asset.hpp>
-
+//#include <map>
 
 using namespace eosio;
 using std::string;
 
-namespace fio {
+namespace fioio {
 
     // @abi table fionames i64
     struct fioname {
 
-	
-	
-		//asset value; // from asset.hpp
         string name = nullptr;
         uint64_t namehash = 0;
         string domain = nullptr;
         uint64_t domainhash = 0;
-		string pubKey = nullptr;
-		uint64_t pubKeyHash = 0;
+		
+		// For debug, not intended for production
+		string pubAddress = nullptr;
+		uint64_t pubAddressHash = 0;
+		/////////////////////////////////////////
+		
 		
         // Chain specific keys
-		// primary_key is required to store structure in multi_index table
+        vector<string> addresses;
+		// std::map<string, string> fionames;
+		
+	// primary_key is required to store structure in multi_index table
         uint64_t primary_key() const { return namehash; }
         uint64_t by_domain() const { return domainhash; }
-		uint64_t by_pubKey() const { return pubKeyHash; }
-		
-		// Any values added to fioname must be added to field below
-        EOSLIB_SERIALIZE(fioname, (name)(namehash)(domain)(domainhash)(pubKey)(pubKeyHash) )
+		uint64_t by_pubAddress() const { return pubAddressHash; }
+        EOSLIB_SERIALIZE(fioname, (name)(namehash)(domain)(domainhash)(addresses)(pubAddress)(pubAddressHash))
     };
 
-	// Where fioname tokens are stored
+	//Where fioname tokens are stored
     typedef multi_index<N(fionames), fioname,
      indexed_by<N(bydomain), const_mem_fun<fioname, uint64_t, &fioname::by_domain> > > fionames_table;
 
@@ -62,6 +62,7 @@ namespace fio {
         uint64_t primary_key() const { return domainhash; }
         EOSLIB_SERIALIZE(domain, (name)(domainhash))
     };
+	
     typedef multi_index<N(domains), domain> domains_table;
 
     struct config {
@@ -70,5 +71,4 @@ namespace fio {
     };
 	
     typedef singleton<N(configs), config> configs;
-
 }
