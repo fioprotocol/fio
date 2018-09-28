bc37f42ce167e4ed55ff7afb4111a2cba4756edf
Ciju John <johnc@objectcomputing.com>
2018-09-27 21:55:11 -0500

    Initial commit of new apis request_funds, cancel_funds_request and approve_request.

--- contracts/fio.name/fio.name.cpp
+++ contracts/fio.name/fio.name.cpp
@@ -13,18 +13,22 @@
  *  Adam Androulidakis  8-29-2019
  */
 
+#include <eosiolib/asset.hpp>
 #include "fio.name.hpp"
 
+#include <climits>
+
 namespace fioio{
-	
-	 
-    class FioNameLookup : public contract { 
+
+
+    class FioNameLookup : public contract {
 		private:
         configs config;
         domains_table domains;
         fionames_table fionames;
 		account_name owner;
-		
+        requests_table pending_requests;
+        requests_table approved_requests;
 
         // Currently supported chains
         enum  class chain_type {
@@ -34,21 +38,24 @@ namespace fioio{
 
     public:
         FioNameLookup(account_name self)
-        : contract(self), config(self, self), domains(self, self), fionames(self, self) {owner=self;}
+                : contract(self), config(self, self), domains(self, self), fionames(self, self),
+                  pending_requests(self, self), approved_requests(self, self)
+        {owner=self;}
 
-    
+
+
         // @abi action
         void registername(const string &name) {
             require_auth(owner);
 			string newname = name;
-			
+
 			// make fioname lowercase before hashing
-			transform(newname.begin(), newname.end(), newname.begin(), ::tolower);	
-			
-			
+			transform(newname.begin(), newname.end(), newname.begin(), ::tolower);
+
+
             string domain = nullptr;
             string fioname = domain;
-			
+
             size_t pos = newname.find('.');
             if (pos == string::npos) {
                 domain = name;
@@ -56,9 +63,9 @@ namespace fioio{
                 fioname = name.substr(0, pos);
                 domain = name.substr(pos + 1, string::npos);
             }
-			
-			
-	
+
+
+
             print("fioname: ", fioname, ", Domain: ", domain, "\n");
 
             uint64_t domainHash = ::eosio::string_to_name(domain.c_str());
@@ -76,27 +83,27 @@ namespace fioio{
                 });
                 // Add domain entry in domain table
             } else { // fioname register
-                
+
 				// check if domain exists.
                 auto domains_iter = domains.find(domainHash);
                 eosio_assert(domains_iter != domains.end(), "Domain not yet registered.");
-                
+
 				// check if domain permission is valid.
-                
+
 				// check if fioname is available
 				uint64_t nameHash = ::eosio::string_to_name(newname.c_str());
                 print("Name hash: ", nameHash, ", Domain has: ", domainHash, "\n");
                 auto fioname_iter = fionames.find(nameHash);
                 eosio_assert(fioname_iter == fionames.end(), "Fioname is already registered.");
-                
 
-				
+
+
 				// check if callee has requisite dapix funds.
 				// DO SOMETHING
-				
+
                 // Issue, create and transfer fioname token
 				// DO SOMETHING
-				
+
                 // Add fioname entry in fionames table
                 fionames.emplace(_self, [&](struct fioname &a){
                     a.name=newname;
@@ -109,7 +116,7 @@ namespace fioio{
         }
 
         /***
-         * Convert chain name to chain type. 
+         * Convert chain name to chain type.
          *
          * @param chain The chain name e.g. "BTC"
          * @return chain_type::NONE if no match.
@@ -128,6 +135,14 @@ namespace fioio{
         }
 
         /***
+         * Validate chain is in the supported chains list.
+         * @param chain The chain to validate, expected to be in lower case.s
+         */
+        inline void assert_valid_chain(const string &chain) {
+            assert(str_to_chain_type(chain) != chain_type::NONE);
+        }
+
+        /***
          * Given a fio user name, chain name and chain specific address will attach address to the user's FIO fioname.
          *
          * @param fio_user_name The FIO user name e.g. "adam.fio"
@@ -141,9 +156,9 @@ namespace fioio{
             eosio_assert(!address.empty(), "Chain address cannot be empty.");
 			// Verify the address does not have a whitespace
 			eosio_assert(address.find(" "), "Chain cannot contain whitespace");
-			
+
 			// DO SOMETHING
-			
+
             // validate chain is supported. This is a case insensitive check.
             string my_chain=chain;
             transform(my_chain.begin(), my_chain.end(), my_chain.begin(), ::toupper);
@@ -160,22 +175,107 @@ namespace fioio{
             fionames.modify(fioname_iter, _self, [&](struct fioname &a) {
                 a.addresses[static_cast<size_t>(c_type)] = address;
             });
-        
+
 		}
-		
-		
+
 		void removename() {
 		}
-		
-		void removedomain() {	
+
+		void removedomain() {
 		}
-		
+
 		void rmvaddress() {
 		}
-		
-		
+
+        /***
+         * User initaites a funds request.
+         */
+        // @abi action
+        void request_funds(uint32_t requestid, const name& from, const name& to, const string &chain, const asset& quantity, const string &memo) {
+            require_auth(from); // we need requesters authorization
+            is_account(to); // ensure requestee exists
+
+            // validate chain is supported. This is a case insensitive check.
+            string my_chain = chain;
+            transform(my_chain.begin(), my_chain.end(), my_chain.begin(), ::toupper);
+            print("Validating chain support: ", my_chain, "\n");
+            assert_valid_chain(my_chain);
+
+            // Validate requestid is unique for this user
+            auto idx = pending_requests.get_index<N(byrequestid)>();
+            auto matchingItem = idx.lower_bound(requestid);
+
+            // Advance to the first entry matching the specified requestid and from
+            while (matchingItem != idx.end() && matchingItem->requestid == requestid &&
+                   matchingItem->from != from) {
+                matchingItem++;
+            }
+
+            // assert identical request from same user doesn't exist
+            assert(matchingItem == idx.end() ||
+                   !(matchingItem->requestid == requestid && matchingItem->from == from));
+
+            // Add fioname entry in fionames table
+            pending_requests.emplace(_self, [&](struct fundsrequest &fr) {
+                fr.requestid = requestid;
+                fr.obtid = 0; // TBD
+                fr.from = from;
+                fr.to = to;
+                fr.chain = chain;
+                fr.quantity = quantity;
+                fr.memo = memo;
+                fr.request_time = now();
+                fr.request_time = UINT_MAX;
+            });
+        }
+
+        /***
+         * Cancel pending funds request
+         */
+        // @abi action
+        void cancel_funds_request(uint16_t requestid, const name& from) {
+            require_auth(from); // we need requesters authorization
+
+            // validate a pending request exists for this requester with this requestid
+            auto idx = pending_requests.get_index<N(byrequestid)>();
+            auto matchingItem = idx.lower_bound(requestid);
+
+            // Advance to the first entry matching the specified vID
+            while (matchingItem != idx.end() && matchingItem->requestid == requestid &&
+                   matchingItem->from != from) {
+                matchingItem++;
+            }
+
+            // assert on match found
+            assert(matchingItem != idx.end() && matchingItem->requestid == requestid &&
+                                 matchingItem->from == from);
+
+            // if match found drop request
+            idx.erase(matchingItem);
+        }
+
+        /***
+         * approve pending funds request
+         */
+        // @abi action
+        void approve_request(uint64_t obtid, const name& requestee) {
+            require_auth(requestee); // we need requesters authorization
+            // validate request obtid exists and is for this requestee
+            auto idx = pending_requests.find(obtid);
+            assert(idx != pending_requests.end());
+            assert(idx->to == requestee);
+
+            // approve request by moving it to approved requests table
+            fundsrequest request = *idx;
+            pending_requests.erase(idx);
+            request.approval_time = now();
+            approved_requests.emplace(_self, [&](struct fundsrequest &fr){
+                fr = request;
+            });
+        }
+
     }; // class FioNameLookup
-	
+
 
     EOSIO_ABI( FioNameLookup, (registername)(addaddress)(removename)(removedomain)(rmvaddress) )
 }
--- contracts/fio.name/fio.name.hpp
+++ contracts/fio.name/fio.name.hpp
@@ -12,20 +12,20 @@
  *  Ciju John  8-30-2018
  *  Adam Androulidakis  8-29-2019
  */
- 
+
 
 #pragma once
 
 #include <eosiolib/eosio.hpp>
 #include <string>
 #include <eosiolib/singleton.hpp>
-//#include <map>
+#include <eosiolib/asset.hpp>
 
 
 using std::string;
 
 namespace fioio {
-    
+
         using namespace eosio;
 
     // @abi table fionames i64
@@ -35,20 +35,39 @@ namespace fioio {
         uint64_t namehash = 0;
         string domain = nullptr;
         uint64_t domainhash = 0;
-		
-		
+
+
         // Chain specific keys
         vector<string> addresses;
 		// std::map<string, string> fionames;
-		
+
 		// primary_key is required to store structure in multi_index table
         uint64_t primary_key() const { return namehash; }
         uint64_t by_domain() const { return domainhash; }
-		
+
         EOSLIB_SERIALIZE(fioname, (name)(namehash)(domain)(domainhash)(addresses))
     };
 
-	//Where fioname tokens are stored
+    // structure for storing funds request
+    struct fundsrequest {
+        uint32_t requestid; // user supplied request id, mainly for user to track requests
+        uint64_t obtid; // FIO Other Blockchain Transaction (OBT) Transaction ID
+        name from; // request originator
+        name to; // requestee
+        string chain;
+        eosio::asset quantity; // requested fund quantity
+        string memo; // user generated memo
+        time  request_time; // request received timestamp
+        time  approval_time; // request approved time stamp
+
+        uint64_t primary_key() const { return obtid; }
+        uint64_t by_requestid() const { return requestid; }
+        EOSLIB_SERIALIZE(fundsrequest, (requestid)(obtid)(from)(to)(chain)(quantity)(memo)(request_time)(approval_time))
+    };
+    typedef multi_index<N(fundrequests), fundsrequest,
+            indexed_by<N(byrequestid), const_mem_fun<fundsrequest, uint64_t, &fundsrequest::by_requestid> > > requests_table;
+
+    //Where fioname tokens are stored
     typedef multi_index<N(fionames), fioname,
      indexed_by<N(bydomain), const_mem_fun<fioname, uint64_t, &fioname::by_domain> > > fionames_table;
 
@@ -60,13 +79,13 @@ namespace fioio {
         uint64_t primary_key() const { return domainhash; }
         EOSLIB_SERIALIZE(domain, (name)(domainhash))
     };
-	
+
     typedef multi_index<N(domains), domain> domains_table;
 
     struct config {
         name tokencontr; // owner of the token contract
         EOSLIB_SERIALIZE(config, (tokencontr))
     };
-	
+
     typedef singleton<N(configs), config> configs;
 }
