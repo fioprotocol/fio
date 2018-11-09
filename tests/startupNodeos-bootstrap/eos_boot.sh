5bd896ca5a3753167cc371b0fab2e2d45b5d8110
Ciju John <johnc@objectcomputing.com>
2018-11-09 13:14:56 -0600

    Rename test launch script and its dependencies to remove name conflict. Update test to reflect name change.

--- tests/startupNodeos-bootstrap/eos_boot.sh
+++ tests/startupNodeos-bootstrap/eos_boot.sh
@@ -12,9 +12,6 @@ if [ "$MY_INTERFACE" == "" ]; then
     MY_INTERFACE=localhost
 fi
 
-# TBD prod conditional
-# PORT=8889
-# read -p "Press any key to setup EOS node to listen on host \"$MY_INTERFACE\" port \"$PORT\""
 PORT=8888
 echo Starting up EOS node to listen on host \"$MY_INTERFACE\" port \"$PORT\"
 #read -p "Press any key to setup EOS node to listen on host \"$MY_INTERFACE\" port \"$PORT\""
