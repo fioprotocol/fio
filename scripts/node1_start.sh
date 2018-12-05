eae2fa695c9fa05a35fa2c510f8da333014b68b5
Adam Androulidakis <37982984+adsorptionenthalpy@users.noreply.github.com>
2018-12-05 10:13:51 -0700

    Revert "Feature/MAS-89"

--- scripts/node1_start.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-nodeos --http-server-address  localhost:8879  --http-validate-host=0 --enable-stale-production --producer-name eosio --plugin eosio::chain_api_plugin --plugin eosio::net_api_plugin
--- scripts/nodeos_kill.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-pkill nodeos
-
-echo "NODEOS KILLED"
