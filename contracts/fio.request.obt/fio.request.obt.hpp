fda1b9f96bcf0cb26a76aa9ec277c840ed5d9a62
cj <cjujohn@gmail.com>
2019-02-07 13:35:13 -0600

    New pub_addr type. Remove OBT tables as we will be using off-chain resources to store OBT data.

--- contracts/fio.request.obt/fio.request.obt.hpp
+++ contracts/fio.request.obt/fio.request.obt.hpp
@@ -14,6 +14,7 @@
 #include <string>
 #include <eosiolib/singleton.hpp>
 #include <eosiolib/asset.hpp>
+#include <eosiolib/fio/types.hpp>
 
 
 using std::string;
@@ -24,8 +25,9 @@ namespace fioio {
 
     // Transaction status
     enum class trxstatus {
-        reject =    0,  // request rejected
-        senttobc =  1   // request send to blockchain
+        requested = 0,  // request sent
+        rejected =  1,  // request rejected
+        senttobc =  2   // payment sent to blockchain
     };
 
     // Structure for "FIO request" context.
@@ -33,8 +35,8 @@ namespace fioio {
     struct fioreqctxt {        // FIO funds request context; specific to requests native to FIO platform
         uint64_t    fioreqid;       // primary key, hash of {fioreqidstr}
         string      fioreqidstr;    // fio funds request id; genetrated by FIO blockchain e.g. 2f9c49bd83632b756efc5184aacf5b41b5ae436189f45c2591ca89248e161c09
-        name        fromfioaddr;    // sender FIO address e.g. john.xyz
-        name        tofioaddr;      // receiver FIO address e.g. jane.xyz
+        pub_addr    fromfioaddr;   // sender FIO address e.g. john.xyz
+        pub_addr    tofioaddr;     // receiver FIO address e.g. jane.xyz
         string      topubaddr;      // chain specific receiver public address e.g 0xC8a5bA5868A5E9849962167B2F99B2040Cee2031
         string      amount;         // token quantity
         string      tokencode;      // token type e.g. BLU
@@ -60,6 +62,7 @@ namespace fioio {
         uint64_t    id;             // primary key, auto-increment
         uint64_t    fioreqid;       // FIO request {fioreqctxt.fioreqid} this request status update is related to
         trxstatus   status;         // request status
+        string      metadata;       // JSON formatted meta data e.g. {"obt_hash": "962167B2F99B20"}
         date        fiotime;        // FIO blockchain status update received timestamp
 
         uint64_t primarykey() const     { return id; }
@@ -72,54 +75,5 @@ namespace fioio {
             indexed_by<N(byfioreqid), const_mem_fun<fioreqsts, uint64_t, &fioreqsts::by_fioreqid> >,
     indexed_by<N(byfiotime), const_mem_fun<fioreqsts, uint64_t, &fioreqsts::by_fiotime> >
     > fiorequest_status_table;
-
-    // Structure for "OBT" context.
-    // @abi table obtctxts i64
-    struct obtctxt {        // Other Blockchain Transaction (OBT) specific data
-        uint64_t    fioobtid;       // primary key, hash of {fioobtidstr}
-        string      fioobtidstr;    // fio OBT id; genetrated by FIO blockchain e.g. 2f9c49bd83632b756efc5184aacf5b41b5ae436189f45c2591ca89248e161c09
-        name        fromfioaddr;    // sender FIO address e.g. john.xyz
-        name        tofioaddr;      // receiver FIO address e.g. jane.xyz
-        string      frompubaddr;    // chain specific sender public address e.g 0xab5801a7d398351b8be11c439e05c5b3259aec9b
-        string      topubaddr;      // chain specific receiver public address e.g 0xC8a5bA5868A5E9849962167B2F99B2040Cee2031
-        string      amount;         // token quantity
-        string      tokencode;      // token type e.g. BLU
-        string      obtidstr;         // OBT id string e.g. 0xf6eaddd3851923f6f9653838d3021c02ab123a4a6e4485e83f5063b3711e000b
-        uint64_t    obtid;          // hash of {obtidstr}
-        string      metadata;       // JSON formatted meta data e.g. {"memo":"utility payment"}
-        date        fiotime;        // FIO blockchain OBT received timestamp
-
-        uint64_t primarykey() const     { return fioobtid; }
-        uint64_t by_sender() const      { return fromfioaddr; }
-        uint64_t by_receiver() const    { return tofioaddr; }
-        uint64_t by_obtid() const       { return obtid; }
-        uint64_t by_fiotime() const     { return fiotime; }
-        EOSLIB_SERIALIZE(obtctxt, (fioobtid)(fioobtidstr)(tofioaddr)(topubaddr)(amount)(tokencode)(obtidstr)(obtid)(metadata)(fiotime))
-    };
-    // FIO OBT table
-    typedef multi_index<N(obtctxts), obtctxt,
-            indexed_by<N(bysender), const_mem_fun<obtctxt, uint64_t, &obtctxt::by_sender> >,
-    indexed_by<N(byreceiver), const_mem_fun<obtctxt, uint64_t, &obtctxt::by_receiver> >,
-    indexed_by<N(byfiotime), const_mem_fun<obtctxt, uint64_t, &obtctxt::by_fiotime> >
-    > obt_contexts_table;
-
-    // Structure for "OBT status" updates.
-    // @abi table obtstss i64
-    struct obtsts {
-        uint64_t    id;             // primary key, auto-increment
-        uint64_t    fioobtid;       // OBT {obtctxt.fioobtid} this OBT status update is related to
-        trxstatus   status;         // OBT status
-        date        fiotime;        // FIO blockchain status update received timestamp
-
-        uint64_t primarykey() const     { return id; }
-        uint64_t by_fioobtid() const     { return fioobtid; }
-        uint64_t by_fiotime() const  { return statustime; }
-        EOSLIB_SERIALIZE(obtsts, (id)(fioobtid)(status)(fiotime))
-    };
-    // OBT status table
-    typedef multi_index<N(obtstss), obtsts,
-            indexed_by<N(byfioobtid), const_mem_fun<obtsts, uint64_t, &obtsts::by_fioobtid> >,
-    indexed_by<N(byfiotime), const_mem_fun<obtsts, uint64_t, &obtsts::by_fiotime> >
-    > obt_status_table;
 
 }
