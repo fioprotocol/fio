602d77169450f2be75105cbc7f74809148f66fb6
Casey Gardiner <casey@dapix.io>
2019-03-11 09:34:19 -0600

    Initial Commit

--- plugins/chain_api_plugin/chain_api_plugin.cpp
+++ plugins/chain_api_plugin/chain_api_plugin.cpp
@@ -110,6 +110,7 @@ void chain_api_plugin::plugin_startup() {
       	CHAIN_RW_CALL_ASYNC(push_transactions, chain_apis::read_write::push_transactions_results, 202),
         CHAIN_RO_CALL(fio_name_lookup, 200),
         CHAIN_RO_CALL(get_pending_fio_requests, 200),
+        CHAIN_RO_CALL(get_sent_fio_requests, 200),
         CHAIN_RO_CALL(fio_key_lookup, 200),
         CHAIN_RW_CALL_ASYNC(register_fio_name, chain_apis::read_write::register_fio_name_results, 202),
         CHAIN_RW_CALL_ASYNC(add_pub_address, chain_apis::read_write::add_pub_address_results, 202),
