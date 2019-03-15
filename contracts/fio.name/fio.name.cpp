1a64f88a0539536eda49342d59956fc43ba442e5
Casey Gardiner <casey@dapix.io>
2019-03-15 17:54:38 -0600

    Refactored registername contract method

--- contracts/fio.name/fio.name.cpp
+++ contracts/fio.name/fio.name.cpp
@@ -44,88 +44,11 @@ namespace fioio {
             FioAddress fa;
             getFioAddressStruct(fioname, fa);
 
-            string nFio = "FIO";
-            uint64_t chainhash = ::eosio::string_to_uint64_t(nFio.c_str());
-            auto chain_iter = chains.find(chainhash);
-
-            if (chain_iter == chains.end()) {
-                chains.emplace(_self, [&](struct chainList &a) {
-                    a.id = 0;
-                    a.chainname = nFio;
-                    a.chainhash = chainhash;
-                });
-            }
-
-            print("fioname = ", fioname, " fa = ", fa.fioaddress, "\n");
             int res = fa.domainOnly ? isFioNameValid(fa.fiodomain) * 10 : isFioNameValid(fa.fioname);
-            print("fioname: ", fa.fioname, ", Domain: ", fa.fiodomain, " error code = ", res, "\n");
-
-            string value = fa.fioaddress + " error = " + to_string(res);
-            fio_400_assert(res == 0, "fio_name", value, "Invalid FIO name format", ErrorInvalidFioNameFormat);
+            fio_400_assert(res == 0, "fio_name", fa.fioaddress, "Invalid FIO name format", ErrorInvalidFioNameFormat);
 
             uint64_t domainHash = ::eosio::string_to_uint64_t(fa.fiodomain.c_str());
-            string registerMemo;
-            uint32_t expiration_time = 0;
-
-            if (fa.domainOnly) { // domain register
-                // check for domain availability
-                auto domains_iter = domains.find(domainHash);
-                fio_400_assert(domains_iter == domains.end(), "fio_name", fa.fioaddress,
-                               "FIO domain already registered", ErrorDomainAlreadyRegistered);
-                // check if callee has requisite dapix funds. Also update to domain fees
-
-                //get the expiration for this new domain.
-                expiration_time = get_now_plus_one_year();
-
-                // Issue, create and transfer nft domain token
-                // Add domain entry in domain table
-                domains.emplace(_self, [&](struct domain &d) {
-                    d.name = fa.fiodomain;
-                    d.domainhash = domainHash;
-                    d.expiration = expiration_time;
-                });
-            } else { // fioname register
-
-                // check if domain exists.
-                auto domains_iter = domains.find(domainHash);
-                fio_400_assert(domains_iter != domains.end(), "fio_name", fa.fioaddress, "FIO Domain not registered",
-                               ErrorDomainNotRegistered);
-
-                // TODO check if domain permission is valid.
-
-                //check if the domain is expired.
-                uint32_t domain_expiration = domains_iter->expiration;
-                uint32_t present_time = now();
-                fio_400_assert(present_time <= domain_expiration, "fio_name", fa.fioaddress, "FIO Domain expired",
-                               ErrorDomainExpired);
-
-                // check if fioname is available
-                uint64_t nameHash = ::eosio::string_to_uint64_t(fa.fioaddress.c_str());
-                print("Name hash: ", nameHash, ", Domain has: ", domainHash, "\n");
-                auto fioname_iter = fionames.find(nameHash);
-                fio_400_assert(fioname_iter == fionames.end(), "fio_name", fa.fioaddress,
-                               "FIO name already registered", ErrorFioNameAlreadyRegistered);
-
-                //set the expiration on this new fioname
-                expiration_time = get_now_plus_one_year();
-
-                // check if callee has requisite dapix funds.
-                // DO SOMETHING
-
-                // Issue, create and transfer fioname token
-                // DO SOMETHING
-
-                // Add fioname entry in fionames table
-                fionames.emplace(_self, [&](struct fioname &a) {
-                    a.name = fa.fioaddress;
-                    a.addresses = vector<string>(20, ""); // TODO: Remove prior to production
-                    a.namehash = nameHash;
-                    a.domain = fa.fiodomain;
-                    a.domainhash = domainHash;
-                    a.expiration = expiration_time;
-                });
-                addaddress(fa.fioaddress, "FIO", actor.to_string(), actor);
-            } // else
+            uint32_t expiration_time = fioaddress_table_update(actor, fa, domainHash, expiration_time);
 
             const auto fees = trxfees.get_or_default(trxfee());
             asset registerFee = fees.upaddress;
@@ -146,12 +69,13 @@ namespace fioio {
         [[eosio::action]]
         void addaddress(const string &fioaddress, const string &tokencode, const string &pubaddress,
                         const account_name &actor) {
-            fio_400_assert(!fioaddress.empty(), "fioaddress", fioaddress, "Invalid public address format",
-                           ErrorDomainAlreadyRegistered);
-            fio_400_assert(!tokencode.empty(), "tokencode", tokencode, "Invalid token code format", ErrorChainEmpty);
+            FioAddress fa;
+            getFioAddressStruct(fioaddress, fa);
 
+            fio_400_assert(isFioNameValid(fa.fioaddress), "fioaddress", fioaddress, "Invalid public address format",
+                           ErrorDomainAlreadyRegistered);
             // Chain input validation
-            fio_400_assert(isChainNameValid(tokencode), "tokencode", tokencode, "Invalid chain format",
+            fio_400_assert(isChainNameValid(tokencode), "tokencode", tokencode, "Invalid token code format",
                            ErrorInvalidFioNameFormat);
             fio_400_assert(isPubAddressValid(pubaddress), "pubaddress", pubaddress, "Invalid public address format",
                            ErrorChainAddressEmpty);
@@ -172,6 +96,7 @@ namespace fioio {
 
             // validate fio FIO Address exists
             uint64_t nameHash = ::eosio::string_to_uint64_t(fioaddress.c_str());
+            uint64_t domainHash = ::eosio::string_to_uint64_t(fa.fiodomain.c_str());
             auto fioname_iter = fionames.find(nameHash);
             fio_404_assert(fioname_iter != fionames.end(), "FIO Address not found", ErrorFioNameNotRegistered);
 
@@ -183,23 +108,11 @@ namespace fioio {
             fio_400_assert(present_time <= name_expiration, "fioaddress", fioaddress,
                            "FIO Address or FIO Domain expired", ErrorFioNameExpired);
 
-            //parse the domain and check that the domain is not expired.
-            string domain = nullptr;
-            size_t pos = fioaddress.find('.');
-
-            if (pos == string::npos) {                                          // TODO Refactor since its used so much
-                eosio_assert(true, "could not find domain name in fio name.");
-            } else {
-                domain = fioaddress.substr(pos + 1, string::npos);
-            }
-
-            uint64_t domainHash = ::eosio::string_to_uint64_t(domain.c_str());
-
             auto domains_iter = domains.find(domainHash);
             fio_404_assert(domains_iter != domains.end(), "FIO Domain not found", ErrorDomainNotRegistered);
 
             uint32_t expiration = domains_iter->expiration;
-            fio_400_assert(present_time <= expiration, "domain", domain, "FIO Address or FIO Domain expired",
+            fio_400_assert(present_time <= expiration, "domain", fa.fiodomain, "FIO Address or FIO Domain expired",
                            ErrorDomainExpired);
 
             // insert/update <chain, address> pair
@@ -213,21 +126,21 @@ namespace fioio {
             auto matchingItem = idx.lower_bound(keyhash);
 
             // Advance to the first entry matching the specified address and chain
-            while (matchingItem != idx.end() && matchingItem->keyhash == keyhash) {
+            while ( matchingItem != idx.end() && matchingItem->keyhash == keyhash ) {
                 matchingItem++;
             }
 
-            if (matchingItem == idx.end() || matchingItem->keyhash != keyhash) {
-                keynames.emplace(_self, [&](struct key_name &k) {
+            if ( matchingItem == idx.end() || matchingItem->keyhash != keyhash ) {
+                keynames.emplace(_self, [&] (struct key_name &k) {
                     k.id = keynames.available_primary_key();        // use next available primary key
                     k.key = pubaddress;                             // persist key
                     k.keyhash = keyhash;                            // persist key hash
-                    k.chaintype = (chain_iter)->by_index();                       // specific chain type
+                    k.chaintype = ( chain_iter )->by_index();                       // specific chain type
                     k.name = fioname_iter->name;                    // FIO name
                     k.expiration = name_expiration;
                 });
             } else {
-                idx.modify(matchingItem, _self, [&](struct key_name &k) {
+                idx.modify(matchingItem, _self, [&] (struct key_name &k) {
                     k.name = fioname_iter->name;    // FIO name
                 });
             }
@@ -244,44 +157,6 @@ namespace fioio {
             send_response(json.dump().c_str());
         }
 
-        inline void fiofees(const account_name &actor, const asset &registerFee) const {
-            if (appConfig.pmtson) {
-                // collect fees
-                // check for funds is implicitly done as part of the funds transfer.
-                print("Collecting registration fees: ", registerFee);
-                action(permission_level{actor, N(active)},
-                       TokenContract, N(transfer),
-                       make_tuple(actor, _self, registerFee,
-                                  string("Registration fees. Thank you."))
-                ).send();
-            } else {
-                print("Payments currently disabled.");
-            }
-        }
-
-        /***
-         * This method will return now plus one year.
-         * the result is the present block time, which is number of seconds since 1970
-         * incremented by secondss per year.
-         */
-        inline uint32_t get_now_plus_one_year() {
-            uint32_t present_time = now();
-            uint32_t incremented_time = present_time + DAYTOSECONDS;
-            return incremented_time;
-        }
-
-        void removename() {
-            print("Begin removename()");
-        }
-
-        void removedomain() {
-            print("Begin removedomain()");
-        }
-
-        void rmvaddress() {
-            print("Begin rmvaddress()");
-        }
-
         /**
          *
          * Separate out the management of platform-specific identities from the fio names
@@ -295,24 +170,124 @@ namespace fioio {
          **/
 
         [[eosio::action]]
-        void bind2eosio(name account, const string &client_key, bool existing) {
+        void bind2eosio (name account, const string &client_key, bool existing) {
             // The caller of this contract must have the private key in their wallet for the FIO.SYSTEM account
             require_auth(::eosio::string_to_name(FIO_SYSTEM));
 
             auto other = eosionames.find(account);
-            if (other != eosionames.end()) {
+            if ( other != eosionames.end()) {
                 eosio_assert_message_code(existing && client_key == other->clientkey, "EOSIO account already bound",
                                           ErrorPubAddressExist);
                 // name in the table and it matches
             } else {
                 eosio_assert_message_code(!existing, "existing EOSIO account not bound to a key", ErrorPubAddressExist);
-                eosionames.emplace(_self, [&](struct eosio_name &p) {
+                eosionames.emplace(_self, [&] (struct eosio_name &p) {
                     p.account = account;
                     p.clientkey = client_key;
                 });
             }
         }
 
+        void removename () {
+            print("Begin removename()");
+        }
+
+        void removedomain () {
+            print("Begin removedomain()");
+        }
+
+        void rmvaddress () {
+            print("Begin rmvaddress()");
+        }
+
+        inline void fiofees(const account_name &actor, const asset &registerFee) const {
+            if (appConfig.pmtson) {
+                // check for funds is implicitly done as part of the funds transfer.
+                print("Collecting registration fees: ", registerFee);
+                action(permission_level{actor, N(active)},
+                       TokenContract, N(transfer),
+                       make_tuple(actor, _self, registerFee,
+                                  string("Registration fees. Thank you."))
+                ).send();
+            } else {
+                print("Payments currently disabled.");
+            }
+        }
+
+        uint32_t fioaddress_table_update (const name &actor, const FioAddress &fa, uint64_t domainHash,
+                                          uint32_t expiration_time) {
+            if ( fa.domainOnly ) { // domain register
+                // check for domain availability
+                auto domains_iter = domains.find(domainHash);
+                fio_400_assert(domains_iter == domains.end(), "fio_name", fa.fioaddress,
+                               "FIO domain already registered", ErrorDomainAlreadyRegistered);
+                // check if callee has requisite dapix funds. Also update to domain fees
+
+                //get the expiration for this new domain.
+                expiration_time = get_now_plus_one_year();
+
+                // Issue, create and transfer nft domain token
+                // Add domain entry in domain table
+                domains.emplace(_self, [&] (struct domain &d) {
+                    d.name = fa.fiodomain;
+                    d.domainhash = domainHash;
+                    d.expiration = expiration_time;
+                });
+            } else { // fioname register
+
+                // check if domain exists.
+                auto domains_iter = domains.find(domainHash);
+                fio_400_assert(domains_iter != domains.end(), "fio_name", fa.fioaddress, "FIO Domain not registered",
+                               ErrorDomainNotRegistered);
+
+                // TODO check if domain permission is valid.
+
+                //check if the domain is expired.
+                uint32_t domain_expiration = domains_iter->expiration;
+                uint32_t present_time = now();
+                fio_400_assert(present_time <= domain_expiration, "fio_name", fa.fioaddress, "FIO Domain expired",
+                               ErrorDomainExpired);
+
+                // check if fioname is available
+                uint64_t nameHash = string_to_uint64_t(fa.fioaddress.c_str());
+                print("Name hash: ", nameHash, ", Domain has: ", domainHash, "\n");
+                auto fioname_iter = fionames.find(nameHash);
+                fio_400_assert(fioname_iter == fionames.end(), "fio_name", fa.fioaddress,
+                               "FIO name already registered", ErrorFioNameAlreadyRegistered);
+
+                //set the expiration on this new fioname
+                expiration_time = get_now_plus_one_year();
+
+                // check if callee has requisite dapix funds.
+                // DO SOMETHING
+
+                // Issue, create and transfer fioname token
+                // DO SOMETHING
+
+                // Add fioname entry in fionames table
+                fionames.emplace(_self, [&] (struct fioname &a) {
+                    a.name = fa.fioaddress;
+                    a.addresses = vector<string>(20, ""); // TODO: Remove prior to production
+                    a.namehash = nameHash;
+                    a.domain = fa.fiodomain;
+                    a.domainhash = domainHash;
+                    a.expiration = expiration_time;
+                });
+                addaddress(fa.fioaddress, "FIO", actor.to_string(), actor);
+            } // else
+
+            return expiration_time;
+        }
+        /***
+         * This method will return now plus one year.
+         * the result is the present block time, which is number of seconds since 1970
+         * incremented by secondss per year.
+         */
+        inline uint32_t get_now_plus_one_year() {
+            uint32_t present_time = now();
+            uint32_t incremented_time = present_time + DAYTOSECONDS;
+            return incremented_time;
+        }
     }; // class FioNameLookup
 
     EOSIO_ABI(FioNameLookup, (registername)(addaddress)(removename)(removedomain)(rmvaddress)(bind2eosio))
