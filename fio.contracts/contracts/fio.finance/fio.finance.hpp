c8b638a31ada0d9d2d2ea3ba2829c2542e7116fe
fio-casey <casey@dapix.io>
2019-05-16 15:19:50 -0600

    Updates to fio.finance contract

--- fio.contracts/contracts/fio.finance/fio.finance.hpp
+++ fio.contracts/contracts/fio.finance/fio.finance.hpp
@@ -77,7 +77,7 @@ namespace fioio {
         uint64_t fioappid;  // key to trxcontext table
         uint16_t type;      // log message of type ${trx_type}
         uint16_t status;    // status message of type ${trx_sts}
-        time time;          // transaction received (by blockchain) time
+        uint32_t time;          // transaction received (by blockchain) time
         string data;        // data binding specific to transaction type. Defined in ${FioFinance::trx_type_dta_*} variables
 
         uint64_t primary_key() const    { return key; }
