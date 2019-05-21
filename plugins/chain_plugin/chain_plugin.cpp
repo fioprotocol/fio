623e7ec38f5ea971c8be0f65841125afd4ba9a69
adsorptionenthalpy <androulidakisa@gmail.com>
2019-05-20 18:00:29 -0600

    record_send reject_funds fix

--- plugins/chain_plugin/chain_plugin.cpp
+++ plugins/chain_plugin/chain_plugin.cpp
@@ -3301,48 +3301,41 @@ chain::plugin_interface::next_function<new_funds_request_results> next) {
 * @return result, result.processed (fc::variant) json blob meeting the api specification.
 */
 void read_write::reject_funds_request(const reject_funds_request_params &params,
-                                      chain::plugin_interface::next_function<reject_funds_request_results> next) {
-    try {
+chain::plugin_interface::next_function<reject_funds_request_results> next) {
+  try {
+     auto pretty_input = std::make_shared<packed_transaction>();
+     auto resolver = make_resolver(this, abi_serializer_max_time);
+     transaction_metadata_ptr ptrx;
+     dlog("reject_funds_request called");
+     try {
+        abi_serializer::from_variant(params, *pretty_input, resolver, abi_serializer_max_time);
+        ptrx = std::make_shared<transaction_metadata>( pretty_input );
+     } EOS_RETHROW_EXCEPTIONS(chain::fio_invalid_trans_exception, "Invalid transaction")
 
-        auto trx = std::make_shared<packed_transaction>();
-        auto resolver = make_resolver(this, abi_serializer_max_time);
-        abi_serializer fio_name_serializer{fc::json::from_string(fio_name_abi).as<abi_def>(),
-                                           abi_serializer_max_time};
+     app().get_method<incoming::methods::transaction_async>()(ptrx, true, [this, next](const fc::static_variant<fc::exception_ptr, transaction_trace_ptr>& result) -> void{
+        if (result.contains<fc::exception_ptr>()) {
+           next(result.get<fc::exception_ptr>());
+        } else {
+           auto trx_trace_ptr = result.get<transaction_trace_ptr>();
 
-        transaction_metadata_ptr ptrx = std::make_shared<transaction_metadata>( trx );
-        name fiosystem = N(fio.reqobtr);
-
-
-        dlog("reject_funds_request called");
-        try {
-            abi_serializer::from_variant(params, *trx, resolver, abi_serializer_max_time);
-        } EOS_RETHROW_EXCEPTIONS(chain::fio_invalid_trans_exception, "Invalid transaction")
-
-        app().get_method<incoming::methods::transaction_async>()(ptrx, true, [this, next](const fc::static_variant<fc::exception_ptr, transaction_trace_ptr>& result) -> void{
-              if (result.contains<fc::exception_ptr>()) {
-                  next(result.get<fc::exception_ptr>());
-              } else {
-                  auto trx_trace_ptr = result.get<transaction_trace_ptr>();
-
-                  try {
-                      fc::variant output;
-                      try {
-                          output = db.to_variant_with_abi(*trx_trace_ptr,
-                                                          abi_serializer_max_time);
-                      } catch (chain::abi_exception &) {
-                          output = *trx_trace_ptr;
-                      }
-                      next(read_write::reject_funds_request_results{output});
-                  } CATCH_AND_CALL(next);
+           try {
+              fc::variant output;
+              try {
+                 output = db.to_variant_with_abi( *trx_trace_ptr, abi_serializer_max_time );
+              } catch( chain::abi_exception& ) {
+                 output = *trx_trace_ptr;
               }
-          });
+
+              next(read_write::reject_funds_request_results{output});
+           } CATCH_AND_CALL(next);
+        }
+     });
 
 
-    } catch (const boost::interprocess::bad_alloc &) {
-        chain_plugin::handle_db_exhaustion();
-    } CATCH_AND_CALL(next);
+  } catch ( boost::interprocess::bad_alloc& ) {
+     chain_plugin::handle_db_exhaustion();
+  } CATCH_AND_CALL(next);
 }
-
 /***
 * record_send - This api method will invoke the fio.request.obt smart contract for recordsend. this api method is
 * intended to add the json passed into this method to the block log so that it can be scraped as necessary.
@@ -3350,45 +3343,40 @@ void read_write::reject_funds_request(const reject_funds_request_params &params,
 * @return result, result.processed (fc::variant) json blob meeting the api specification.
 */
 void read_write::record_send (const record_send_params &params,
-                              chain::plugin_interface::next_function<record_send_results> next) {
-    try {
-        auto trx = std::make_shared<packed_transaction>();
-        auto resolver = make_resolver(this, abi_serializer_max_time);
-        abi_serializer fio_name_serializer{fc::json::from_string(fio_name_abi).as<abi_def>(),
-                                           abi_serializer_max_time};
+chain::plugin_interface::next_function<record_send_results> next) {
+  try {
+     auto pretty_input = std::make_shared<packed_transaction>();
+     auto resolver = make_resolver(this, abi_serializer_max_time);
+     transaction_metadata_ptr ptrx;
+     dlog("record_send called");
+     try {
+        abi_serializer::from_variant(params, *pretty_input, resolver, abi_serializer_max_time);
+        ptrx = std::make_shared<transaction_metadata>( pretty_input );
+     } EOS_RETHROW_EXCEPTIONS(chain::fio_invalid_trans_exception, "Invalid transaction")
 
-        transaction_metadata_ptr ptrx = std::make_shared<transaction_metadata>( trx );
-        name fiosystem = N(fio.reqobtr);
+     app().get_method<incoming::methods::transaction_async>()(ptrx, true, [this, next](const fc::static_variant<fc::exception_ptr, transaction_trace_ptr>& result) -> void{
+        if (result.contains<fc::exception_ptr>()) {
+           next(result.get<fc::exception_ptr>());
+        } else {
+           auto trx_trace_ptr = result.get<transaction_trace_ptr>();
 
-        dlog("record_send called");
-        try {
-            abi_serializer::from_variant(params, *trx, resolver, abi_serializer_max_time);
-        } EOS_RETHROW_EXCEPTIONS(chain::fio_invalid_trans_exception, "Invalid transaction")
-
-
-        app().get_method<incoming::methods::transaction_async>()(ptrx, true, [this, next](const fc::static_variant<fc::exception_ptr, transaction_trace_ptr>& result) -> void{
-                if (result.contains<fc::exception_ptr>()) {
-                  next(result.get<fc::exception_ptr>());
-              } else {
-                  auto trx_trace_ptr = result.get<transaction_trace_ptr>();
-
-                  try {
-
-                      fc::variant output;
-                      try {
-                          output = db.to_variant_with_abi(*trx_trace_ptr,
-                                                          abi_serializer_max_time);
-                      } catch (chain::abi_exception &) {
-                          output = *trx_trace_ptr;
-                      }
-                      next(read_write::record_send_results{output});
-                  } CATCH_AND_CALL(next);
+           try {
+              fc::variant output;
+              try {
+                 output = db.to_variant_with_abi( *trx_trace_ptr, abi_serializer_max_time );
+              } catch( chain::abi_exception& ) {
+                 output = *trx_trace_ptr;
               }
-          });
 
-    } catch (const boost::interprocess::bad_alloc &) {
-        chain_plugin::handle_db_exhaustion();
-    } CATCH_AND_CALL(next);
+              next(read_write::record_send_results{output});
+           } CATCH_AND_CALL(next);
+        }
+     });
+
+
+  } catch ( boost::interprocess::bad_alloc& ) {
+     chain_plugin::handle_db_exhaustion();
+  } CATCH_AND_CALL(next);
 }
 
 /***
