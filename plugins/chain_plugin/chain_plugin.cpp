e4b121585fcf7f65006537b2fd632ca49803e330
adsorptionenthalpy <androulidakisa@gmail.com>
2019-10-08 16:16:30 -0600

    add_pub_address alter Invalid Token Code assertion

--- plugins/chain_plugin/chain_plugin.cpp
+++ plugins/chain_plugin/chain_plugin.cpp
@@ -2273,8 +2273,9 @@ if( options.count(name) ) { \
 
             chains_result = get_table_rows_ex<key_value_index>(chain_table_row_params, abi);
 
-            FIO_404_ASSERT(!chains_result.rows.empty(), "Invalid Token Code", fioio::ErrorPubAddressNotFound);
-
+            FIO_400_ASSERT(!chains_result.rows.empty(), "token_code", p.token_code,
+                           "Invalid Token Code",
+                           fioio::ErrorTokenCodeInvalid);
             //   // Pick out chain specific key and populate result
             uint32_t c_type = (uint32_t) chains_result.rows[0]["id"].as_uint64();
             result.public_address = name_result.rows[0]["addresses"][static_cast<int>(c_type)].as_string();
