5a47ddd958753abeaffce8fa213988d41c600176
Ed Rotthoff <ed@dapix.io>
2019-10-12 17:56:21 -0600

    revise comments in the fio.request.obt contract

--- fio.contracts/contracts/fio.request.obt/fio.request.obt.hpp
+++ fio.contracts/contracts/fio.request.obt/fio.request.obt.hpp
@@ -1,5 +1,7 @@
 /** FIO Request OBT header file
- *  Description: Smart contract to track requests and OBT.
+ *  Description: The FIO request other blockchain transaction (or obt) contract contains the
+ *  tables that hold the requests for funds that have been made along with the status of any
+ *  response to each request for funds.
  *  @author Adam Androulidakis, Casey Gardiner, Ciju John, Ed Rotthoff
  *  @file fio.request.obt.hpp
  *  @copyright Dapix
@@ -19,14 +21,16 @@ namespace fioio {
 
     using namespace eosio;
 
-    // Transaction status
+    // The status of a transaction progresses from requested, to either rejected or sent to blockchain (if funds
+    // are sent in response to the request.
     enum class trxstatus {
         requested = 0,
         rejected = 1,
         sent_to_blockchain = 2
     };
 
-    // Structure for "FIO request" context.
+    // The request context table holds the requests for funds that have been requested, it provides
+    // searching by id, payer and payee.
     // @abi table fioreqctxts i64
     struct [[eosio::action]] fioreqctxt {
         uint64_t fio_request_id;
@@ -34,7 +38,7 @@ namespace fioio {
         uint128_t payee_fio_address;
         string payer_fio_address_hex_str;
         string payee_fio_address_hex_str;
-        string content;
+        string content;  //this content is a encrypted blob containing the details of the request.
         uint64_t time_stamp;
         string payer_fio_addr;
         string payee_fio_addr;
@@ -49,20 +53,21 @@ namespace fioio {
         )
     };
 
-    // FIO requests contexts table
+
     typedef multi_index<"fioreqctxts"_n, fioreqctxt,
             indexed_by<"byreceiver"_n, const_mem_fun < fioreqctxt, uint128_t, &fioreqctxt::by_receiver>>,
     indexed_by<"byoriginator"_n, const_mem_fun<fioreqctxt, uint128_t, &fioreqctxt::by_originator>
     >>
     fiorequest_contexts_table;
 
-    // Structure for "FIO request status" updates.
+    // The FIO request status table references FIO requests that have had funds sent, or have been rejected.
+    // the table provides a means to find the status of a request.
     // @abi table fioreqstss i64
     struct [[eosio::action]] fioreqsts {
         uint64_t id;
         uint64_t fio_request_id;
         uint64_t status;
-        string metadata;
+        string metadata;   //this contains a json string with meta data regarding the response to a request.
         uint64_t time_stamp;
 
         uint64_t primary_key() const { return id; }
@@ -72,7 +77,6 @@ namespace fioio {
         )
     };
 
-    // FIO requests status table
     typedef multi_index<"fioreqstss"_n, fioreqsts,
             indexed_by<"byfioreqid"_n, const_mem_fun < fioreqsts, uint64_t, &fioreqsts::by_fioreqid> >
     >
