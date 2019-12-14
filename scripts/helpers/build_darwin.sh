442a21015c135fca733021cdcf3b3857e141b7fd
Casey Gardiner <casey@dapix.io>
2019-12-14 09:28:08 -0700

    llvm bash fix

--- scripts/helpers/build_darwin.sh
+++ scripts/helpers/build_darwin.sh
@@ -31,12 +31,12 @@ fi
 
 echo "${COLOR_CYAN}[Checking LLVM 4 support]${COLOR_NC}"
 if [ ! -d $LLVM_ROOT ]; then
-	cd /usr/local/Cellar
-	wget https://bin.fioprotocol.io/build-tools/llvm4-mac.tbz
-  tar jxf llvm4-mac.tbz
-  cd /usr/local/opt
-  ln -s ../Cellar/llvm@4/4.0.1_1 llvm@4
-  brew pin llvm@4
+  execute bash -c "cd /usr/local/Cellar && \
+	curl -OL https://bin.fioprotocol.io/build-tools/llvm4-mac.tbz \
+  && tar jxf llvm4-mac.tbz \
+  && cd /usr/local/opt \
+  && ln -s ../Cellar/llvm@4/4.0.1_1 llvm@4 \
+  && brew pin llvm@4"
 	printf " - LLVM successfully linked from /usr/local/opt/llvm@4 to ${LLVM_ROOT}\\n"
 else
 	printf " - LLVM found @ ${LLVM_ROOT}.\\n"
