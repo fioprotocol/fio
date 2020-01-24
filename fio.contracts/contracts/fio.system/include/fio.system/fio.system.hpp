21ef189c9e668c4dc801de03f0d1b8d9de3a573e
adsorptionenthalpy <androulidakisa@gmail.com>
2020-01-23 20:37:40 -0700

    move fee updates ahead of elected producer update

--- fio.contracts/contracts/fio.system/include/fio.system/fio.system.hpp
+++ fio.contracts/contracts/fio.system/include/fio.system/fio.system.hpp
@@ -80,12 +80,13 @@ struct [[eosio::table("global"), eosio::contract("fio.system")]] eosio_global_st
     uint16_t last_producer_schedule_size = 0;
     double total_producer_vote_weight = 0; /// the sum of all producer votes
     block_timestamp last_name_close;
+    block_timestamp last_fee_update;
 
     // explicit serialization macro is not necessary, used here only to improve compilation time
     EOSLIB_SERIALIZE_DERIVED( eosio_global_state, eosio::blockchain_parameters,
             (last_producer_schedule_update)(last_pervote_bucket_fill)
             (pervote_bucket)(perblock_bucket)(total_unpaid_blocks)(total_voted_fio)(thresh_voted_fio_time)
-            (last_producer_schedule_size)(total_producer_vote_weight)(last_name_close)
+            (last_producer_schedule_size)(total_producer_vote_weight)(last_name_close)(last_fee_update)
     )
 };
 
