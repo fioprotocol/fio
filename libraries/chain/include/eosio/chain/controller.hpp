d169c9bd38fc632ab0b30621299b6199272d0826
adsorptionenthalpy <androulidakisa@gmail.com>
2020-03-03 16:04:34 -0700

    fix immutable mode - 1.8.11 - https://github.com/EOSIO/eos/pull/8566

--- libraries/chain/include/eosio/chain/controller.hpp
+++ libraries/chain/include/eosio/chain/controller.hpp
@@ -55,6 +55,10 @@ namespace eosio {
             IRREVERSIBLE
         };
 
+        inline bool db_mode_is_immutable(db_read_mode m) {
+          return db_read_mode::READ_ONLY == m || db_read_mode::IRREVERSIBLE ==m;
+        }
+
         enum class validation_mode {
             FULL,
             LIGHT
@@ -343,6 +347,8 @@ namespace eosio {
             db_read_mode get_read_mode() const;
 
             validation_mode get_validation_mode() const;
+
+            bool in_immutable_mode() const;
 
             void set_subjective_cpu_leeway(fc::microseconds leeway);
 
--- plugins/net_plugin/net_plugin.cpp
+++ plugins/net_plugin/net_plugin.cpp
@@ -2547,7 +2547,7 @@ namespace eosio {
       fc_dlog(logger, "got a packed transaction, cancel wait");
       peer_ilog(c, "received packed_transaction");
       controller& cc = my_impl->chain_plug->chain();
-      if( cc.get_read_mode() == eosio::db_read_mode::READ_ONLY ) {
+      if( db_mode_is_immutable(cc.get_read_mode()) ) {
          fc_dlog(logger, "got a txn in read-only mode - dropping");
          return;
       }
@@ -3115,7 +3115,7 @@ namespace eosio {
 
       my->incoming_transaction_ack_subscription = app().get_channel<channels::transaction_ack>().subscribe(boost::bind(&net_plugin_impl::transaction_ack, my.get(), _1));
 
-      if( cc.get_read_mode() == chain::db_read_mode::READ_ONLY ) {
+      if( cc.in_immutable_mode() ) {
          my->max_nodes_per_host = 0;
          fc_ilog( logger, "node in read-only mode setting max_nodes_per_host to 0 to prevent connections" );
       }
