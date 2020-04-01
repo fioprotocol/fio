ee081dfd90a23412c68206862aefe12a42aeecfc
Ed Rotthoff <ed@dapix.io>
2020-04-01 13:57:37 -0600

    set to seconds per day

--- fio.contracts/contracts/fio.treasury/fio.treasury.cpp
+++ fio.contracts/contracts/fio.treasury/fio.treasury.cpp
@@ -11,9 +11,7 @@
 #define BPMAXTOMINT     50000000000000          // 50,000  FIO
 #define FDTNMAXRESERVE  181253654000000000      // 181,253,654 FIO
 #define BPMAXRESERVE    10000000000000000       // 10,000,000 FIO
-//#define PAYSCHEDTIME    172801                  // 1 day  ( block time )
-//kludge to make pay schedule shorter for bpclaim testing
-#define PAYSCHEDTIME    120                  // 2 minutes  ( block time )
+#define PAYSCHEDTIME    86401                   // seconds per day + 1
 #define PAYABLETPIDS    100
 
 #include "fio.treasury.hpp"
