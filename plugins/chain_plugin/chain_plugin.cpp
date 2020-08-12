be1ffe9d171826fb6b70dd11d472fbc1009f7ac7
Casey Gardiner <44983607+dapixcasey@users.noreply.github.com>
2020-08-12 10:55:06 -0600

    Update chain_plugin.cpp

--- plugins/chain_plugin/chain_plugin.cpp
+++ plugins/chain_plugin/chain_plugin.cpp
@@ -5507,7 +5507,7 @@ if( options.count(name) ) { \
         read_only::serialize_json(const read_only::serialize_json_params &params) const try {
             serialize_json_result result;
 
-            const int32_t HF1_BLOCK_TIME = 1596729600; //Aug 6 2020 10am MST 4pm UTC, test net forking deadline
+            const int32_t HF1_BLOCK_TIME = 1600876800; //Aug 6 2020 10am MST 4pm UTC, test net forking deadline
             string actionname;
 
             action_name nm = params.action;
