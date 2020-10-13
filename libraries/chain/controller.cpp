8ce66f23a8ae3942c76dde78f0e6912435243367
Ed Rotthoff <ed@dapix.io>
2020-10-13 13:45:00 -0600

    formatting

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -1036,6 +1036,8 @@ namespace eosio {
                                                                                   majority_permission.id,
                                                                                   active_producers_authority,
                                                                                   conf.genesis.initial_timestamp);
+
+                
                //these actions are added to the action mapping here to permit the launch of
                //test networks for development testing and private test net testing.
                //we put the actions into the table here and they are initialized for use
