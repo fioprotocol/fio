10c0606c80beb6670adb8355ccc4c8212ed4503b
adsorptionenthalpy <androulidakisa@gmail.com>
2021-02-22 18:16:04 -0700

    remove subcommands supporting fee setting

--- programs/clio/main.cpp
+++ programs/clio/main.cpp
@@ -1272,57 +1272,6 @@ struct vote_producers_subcommand {
     }
 };
 
-struct fee_amount_subcommand {
-    string actor;
-    string maxfee_str;
-    string fioaddress_str;
-    vector<string> fee_ratios;
-
-     fee_amount_subcommand(CLI::App *actionRoot) {
-        auto setAmount = actionRoot->add_subcommand("submit_ratios", localized("Vote for one or more producers"));
-        setAmount->add_option("fio_address", fioaddress_str, localized("The voting fio address"))->required();
-        setAmount->add_option("actor", actor, localized("The voting fio account"))->required();
-        setAmount->add_option("max_fee", maxfee_str, localized("The maximum fio fee to pay while voting"))->required();
-        setAmount->add_option("fee_ratios", fee_ratios, localized(
-                "The fees to vote on. All options from this position and following will be treated as the fee list."))->required();
-        add_standard_transaction_options(setAmount, "voter@active");
-
-        setAmount->set_callback([this] {
-
-            fc::variant act_payload = fc::mutable_variant_object()
-                    ("fee_ratios", fee_ratios)
-                    ("fio_address",fioaddress_str)
-                    ("actor", actor)
-                    ("max_fee",maxfee_str);
-            auto accountPermissions = get_account_permissions(tx_permission, {actor, config::active_name});
-            send_actions(
-                    {create_action(accountPermissions, N(fio.fee), N(setfeevote), act_payload)});
-        });
-    }
-};
-
-struct fee_multiplier_subcommand {
-    string actor;
-    string multiplier;
-
-     fee_multiplier_subcommand(CLI::App *actionRoot) {
-        auto setMult = actionRoot->add_subcommand("submit_multiplier", localized("Set the fee multiplier"));
-        setMult->add_option("multiplier", multiplier, localized("The fee multiplier to set"))->required();
-        setMult->add_option("actor", actor, localized("The actor setting the multiplier"))->required();
-        add_standard_transaction_options(setMult, "actor@active");
-
-        setMult->set_callback([this] {
-
-            fc::variant act_payload = fc::mutable_variant_object()
-                    ("actor", actor)
-                    ("multiplier", multiplier);
-            auto accountPermissions = get_account_permissions(tx_permission, {actor, config::active_name});
-            send_actions(
-                    {create_action(accountPermissions, N(fio.fee), N(setfeemult), act_payload)});
-        });
-    }
-};
-
 //NOTE -- fix these up after main net launch, the producer accounts are in the votes table, we need
 //         to get the associated producer fio address and use a vector of fio addresses instead of names
 //         then do the final call to vote producers.
@@ -4721,8 +4670,6 @@ int main(int argc, char **argv) {
    // auto unapproveProducer = unapprove_producer_subcommand(voteProducer);
    auto fee = app.add_subcommand("fee", localized("Interact with the fio.fee contract."), false);
    fee->require_subcommand();
-   auto setAmount = fee_amount_subcommand(fee);
-   auto setMult = fee_multiplier_subcommand(fee);
 
     auto listProducers = list_producers_subcommand(system);
 
