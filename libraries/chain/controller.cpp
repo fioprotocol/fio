120c5caa6dc02f91c4dbc5d067decd55fedbafe7
Eric Butz <ericgbutz@gmail.com>
2021-09-29 08:43:11 -0600

    Comment out bootstrap actions for release

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -1037,6 +1037,7 @@ namespace eosio {
                                                                                   active_producers_authority,
                                                                                   conf.genesis.initial_timestamp);
 
+            /*
                 //these actions are added to the action mapping here to permit the launch of
                 //test networks for development testing and private test net testing.
                 //we put the actions into the table here and they are initialized for use
@@ -1380,7 +1381,7 @@ namespace eosio {
                 });
 
             }
-
+        */
 
             // The returned scoped_exit should not exceed the lifetime of the pending which existed when make_block_restore_point was called.
             fc::scoped_exit<std::function<void()>> make_block_restore_point() {
